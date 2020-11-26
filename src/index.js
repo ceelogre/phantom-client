7ee93b6cf245723a33c6bb1d7a7e5c36d59a70f3
ceelogre <sam.nishimwe@yahoo.com>
2020-11-26 22:31:04 +0200

    ch(setup) add startup source code

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import react from 'react'
+import ReactDOM from 'react-dom'
+
+export default () => {
+  ReactDOM.render(
+      <h1>Hello Spot</h1>
+    , document.getElementById('body-container')
+  )
+}
