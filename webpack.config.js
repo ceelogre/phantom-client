7266c6b1c1d99c1970a829659447b788420c5ef5
ceelogre <sam.nishimwe@yahoo.com>
2020-12-08 16:13:00 +0200

    ch(setup) webpack basic config

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,10 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { Template } = require('webpack')
 
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
+  mode: 'development',
   module: {
     rules: [
       {
@@ -15,9 +18,19 @@ module.exports = {
     extensions: ['*', '.js', '.jsx']
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'main.js'
   },
   devServer: {
-    contentBase: path.resolve(__dirname, './dist')
-  }
+    contentBase: path.resolve(__dirname, 'dist'),
+    //open: true, //Immediatelys makes the app available at a given port
+    //publicPath: '/dist', DEV
+  },
+  plugins: [
+    // Creates an html starter page
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src/index.html')
+    }),
+  ]
 }
